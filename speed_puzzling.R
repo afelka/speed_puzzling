73c58dd2c0847b5d49d4b12749d430b68acf95a6
afelka <erdememin@gmail.com>
2024-06-01 21:04:38 +0200

    Create manual name-url table, scrape information and create a gglot

---
+++ speed_puzzling.R
@@ -0,0 +1,146 @@
+library(rvest)
+library(tidyr)
+library(dplyr)
+library(stringr)
+library(stringi)
+library(lubridate)
+library(ggplot2)
+library(ggimage)
+
+#Create table manually for the names and urls of different competitions 
+urls <- data.frame(name = c("WPC 2019",
+                            "WPC 2022 - Qualifying A",
+                            "WPC 2022 - Qualifying B",
+                            "WPC 2022 - Qualifying C",
+                            "WPC 2022 - Final",
+                            "WPC 2023 - First Round A",
+                            "WPC 2023 - First Round B",
+                            "WPC 2023 - First Round C",
+                            "WPC 2023 - First Round D",
+                            "WPC 2023 - First Round E",
+                            "WPC 2023 - First Round F",
+                            "WPC 2023 - Semi Final 1",
+                            "WPC 2023 - Semi Final 2",
+                            "WPC 2023 - Final",
+                            "Australia 2022",
+                            "Australia 2023",
+                            "Canada 2023 - First Round",
+                            "Canada 2023 - Final",
+                            "Canada 2024 - First Round A",
+                            "Canada 2024 - First Round B",
+                            "Canada 2024 - Final",
+                            "Denmark 2024 - First Round A",
+                            "Denmark 2024 - First Round B",
+                            "Sweden 2023",
+                            "Sweden 2024 - First Round A",
+                            "Sweden 2024 - First Round B",
+                            "Sweden 2024 - Final",
+                            "Spain 2023",
+                            "US National 2022",
+                            "US National 2024 - First Round A",
+                            "US National 2024 - First Round B",
+                            "US National 2024 - First Round C",
+                            "US National 2024 - Final"                            ),
+                   url = c("https://www.worldjigsawpuzzle.org/wjpc/2019/individual/final",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2022/individual/A",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2022/individual/B",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2022/individual/C",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2022/individual/final",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2023/individual/A",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2023/individual/B",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2023/individual/C",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2023/individual/D",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2023/individual/E",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2023/individual/F",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2023/individual/S1",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2023/individual/S2",
+                           "https://www.worldjigsawpuzzle.org/wjpc/2023/individual/final",
+                           "https://www.worldjigsawpuzzle.org/ajpa/nationals2022/individual",
+                           "https://www.worldjigsawpuzzle.org/ajpa/nationals2023/individual",
+                           "https://www.worldjigsawpuzzle.org/cjpa/nationals2023/individual",
+                           "https://www.worldjigsawpuzzle.org/cjpa/nationals2023/individual/final",
+                           "https://www.worldjigsawpuzzle.org/cjpa/nationals2024/individual/A",
+                           "https://www.worldjigsawpuzzle.org/cjpa/nationals2024/individual/B",
+                           "https://www.worldjigsawpuzzle.org/cjpa/nationals2024/individual/final",
+                           "https://www.worldjigsawpuzzle.org/danskpuslespilsforening/2024/individual/A",
+                           "https://www.worldjigsawpuzzle.org/danskpuslespilsforening/2024/individual/B",
+                           "https://www.worldjigsawpuzzle.org/svenskapusselforbundet/2023/individual",
+                           "https://www.worldjigsawpuzzle.org/svenskapusselforbundet/2024/individual/A",
+                           "https://www.worldjigsawpuzzle.org/svenskapusselforbundet/2024/individual/B",
+                           "https://www.worldjigsawpuzzle.org/svenskapusselforbundet/2024/individual/final",
+                           "https://www.worldjigsawpuzzle.org/aepuzz/nationals2023/individual",
+                           "https://www.worldjigsawpuzzle.org/usajpa/nationals2022/individual",
+                           "https://www.worldjigsawpuzzle.org/usajpa/nationals2024/individual/A",
+                           "https://www.worldjigsawpuzzle.org/usajpa/nationals2024/individual/B",
+                           "https://www.worldjigsawpuzzle.org/usajpa/nationals2024/individual/C",
+                           "https://www.worldjigsawpuzzle.org/usajpa/nationals2024/individual/final"
+                           ))
+      
+
+#function for testing whether we can download a given url
+readUrl <- function(url) {
+  out <- tryCatch(
+    {
+      download.file(url, destfile = "scrapedpage.html", quiet = TRUE)
+      return(1)
+    },
+    error = function(cond) {
+      return(0)
+    },
+    warning = function(cond) {
+      return(0)
+    }
+  )
+  return(out)
+}
+
+#create an empty data.frame
+total_list <- data.frame(name = character(),
+                         image_name = character(),
+                         top_20_average = character())
+
+# go through the urls one by one and get average minutes for top 20 contestants and 
+# download the images
+for(i in seq_len(nrow(urls))) {
+
+url <- urls$url[i]
+readUrl(url)
+
+content <- read_html("scrapedpage.html")
+
+times_by_contestants <- content %>% 
+  html_nodes(xpath = '//div[@class="tiempo"]') %>%
+  html_text() %>% as.data.frame()
+
+top_20_average <- times_by_contestants %>% head(20) %>% 
+                  mutate(time_in_minutes = hour(hms(gsub(" ","", .)))*60 +
+                         minute(hms(gsub(" ","", .)))) %>% select(time_in_minutes) %>%
+                  summarise(avg_minutes = mean(time_in_minutes))
+
+image_url <- paste0("https://www.worldjigsawpuzzle.org/users/", content %>%
+  html_nodes(xpath = '//img[@class="puzzle_unico"]') %>%
+  html_attr("src"))
+
+image_name <- paste0(gsub(" ","_",str_to_lower(gsub("[^[:alnum:][:space:]]","",urls$name[i]))),".jpg")
+
+download.file(image_url, destfile = image_name ,mode = "wb")
+
+temp_data_frame <- data.frame(name =  urls$name[i], image_name, top_20_average)
+
+total_list <<-  rbind(total_list , temp_data_frame)
+
+}
+
+#create ggplot, when reordering by minutes, images might overlap, therefore I left it as it is
+total_list_plot <- 
+  ggplot(total_list, aes(y = name, x = avg_minutes)) +
+  geom_image(aes(image = image_name), size = 0.08) +
+  labs(title = "Speed Puzzling - Avg. Minutes Spent by top 20 Contestants Across Competitions") +
+  theme_classic() +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_text(face = "bold"),
+        plot.title = element_text(size = 7)) 
+
+#save ggplot
+ggsave("speed_puzzling.png", plot = total_list_plot, width = 6, height = 4, dpi = 300)
